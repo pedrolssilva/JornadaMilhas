17b771951e0846546609cce9a382246778970b6e
Pedro Luis <pedrolssilva@hotmail.com>
2024-03-25 19:45:03 -0300

    Implement discount feature with TDD

--- src/JornadaMilhasV1/Modelos/OfertaViagem.cs
+++ src/JornadaMilhasV1/Modelos/OfertaViagem.cs
@@ -7,14 +7,23 @@ using JornadaMilhasV1.Validador;
 
 namespace JornadaMilhasV1.Modelos;
 
-public class OfertaViagem: Valida
+public class OfertaViagem : Valida
 {
+    private double desconto;
+
     public int Id { get; set; }
-    public Rota Rota { get; set; } 
+    public Rota Rota { get; set; }
     public Periodo Periodo { get; set; }
     public double Preco { get; set; }
-
-
+    public double Desconto
+    {
+        get => desconto;
+        set
+        {
+            desconto = value;
+            Preco -= desconto;
+        }
+    }
     public OfertaViagem(Rota rota, Periodo periodo, double preco)
     {
         Rota = rota;
@@ -33,10 +42,11 @@ public class OfertaViagem: Valida
         if (!Periodo.EhValido)
         {
             Erros.RegistrarErro(Periodo.Erros.Sumario);
-        } else if (Rota == null || Periodo == null)
+        }
+        else if (Rota == null || Periodo == null)
         {
             Erros.RegistrarErro("A oferta de viagem não possui rota ou período válidos.");
-        } 
+        }
         else if (Preco <= 0)
         {
             Erros.RegistrarErro("O preço da oferta de viagem deve ser maior que zero.");
---
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,31 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            //Arrange 
+            Rota rota = new Rota("OrigemA", "DestinoB");
+            Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double precoOriginal = 100.00;
+            double desconto = 20.00;
+            double precoComDesconto = precoOriginal - desconto;
+
+            OfertaViagem oferta = new(rota, periodo, precoOriginal);
+
+            //Act 
+            oferta.Desconto = desconto;
+
+            //Assert
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+    }
+}
