848920890acabd980ed92349beb47c49e7650f7f
Pedro Luis <pedrolssilva@hotmail.com>
2024-05-06 19:50:25 -0300

    Create test for RecuperarTodas method

---
+++ JM.Test.Integracao/OfertaViagemDalRecuperarTodas.cs
@@ -0,0 +1,35 @@
+﻿using JornadaMilhas.Dados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace JM.Test.Integracao
+{
+    [Collection(nameof(ContextoCollection))]
+    public class OfertaViagemDalRecuperarTodas
+    {
+        private readonly JornadaMilhasContext context;
+       
+        public OfertaViagemDalRecuperarTodas(ITestOutputHelper output, ContextoFixture fixture)
+        {
+            context = fixture.Context;
+            output.WriteLine(context.GetHashCode().ToString());
+        }
+
+        [Fact]
+        public void RetornaListaDeOferta()
+        {
+            //Arrange 
+            var dal = new OfertaViagemDAL(context);
+
+            //Act 
+            var ofertaRecuperada = dal.RecuperarTodas();
+
+            //Assert    
+            Assert.NotEmpty(ofertaRecuperada);
+        }
+    }
+}
