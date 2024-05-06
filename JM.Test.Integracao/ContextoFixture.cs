bb20032d4aa3d9e3efc0c5f6ef466f707d8d2dfc
Pedro Luis <pedrolssilva@hotmail.com>
2024-05-06 20:06:30 -0300

    Using TestContainers library to simulate a local database with docker

--- JM.Test.Integracao/ContextoFixture.cs
+++ JM.Test.Integracao/ContextoFixture.cs
@@ -5,20 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JM.Test.Integracao
 {
-    public class ContextoFixture
+    public class ContextoFixture : IAsyncLifetime
     {
-        public JornadaMilhasContext Context { get; }
+        public JornadaMilhasContext Context { get; private set; }
+        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+            .Build();
 
-        public ContextoFixture()
+        public async Task InitializeAsync()
         {
+            await _msSqlContainer.StartAsync();
             var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-                .UseSqlServer("Server = localhost, 1433; Database = JornadaMilhas; User Id = sa; Password = UoyN^bW61K; Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+                .UseSqlServer(_msSqlContainer.GetConnectionString())
+                //.UseSqlServer("Server = localhost, 1433; Database = JornadaMilhas; User Id = sa; Password = UoyN^bW61K; Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
                 .Options;
 
             Context = new JornadaMilhasContext(options);
+            Context.Database.Migrate();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _msSqlContainer.StopAsync();
         }
     }
 }
