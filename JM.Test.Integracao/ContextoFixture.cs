c369f90663b639e6eac034134b63cbcd5f7804ec
Pedro Luis <pedrolssilva@hotmail.com>
2024-05-13 20:23:55 -0300

    Use bogus to generate test data and share mass data between tests

--- JM.Test.Integracao/ContextoFixture.cs
+++ JM.Test.Integracao/ContextoFixture.cs
@@ -1,4 +1,6 @@
-﻿using JornadaMilhas.Dados;
+﻿using Bogus;
+using JornadaMilhas.Dados;
+using JornadaMilhasV1.Modelos;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,30 @@ namespace JM.Test.Integracao
 
             Context = new JornadaMilhasContext(options);
             Context.Database.Migrate();
+        }
+
+        public void CriaDadosFake()
+        {
+            var fakerPeriodo = new Faker<Periodo>()
+               .CustomInstantiator(f =>
+               {
+                   DateTime dataInicio = f.Date.Soon();
+                   return new Periodo(dataInicio, dataInicio.AddDays(30));
+               });
+            var rota = new Rota("Curitiba", "São Paulo");
+
+            var fakerOferta = new Faker<OfertaViagem>()
+               .CustomInstantiator(f => new OfertaViagem(
+                   rota,
+                   fakerPeriodo.Generate(),
+                   100 * f.Random.Int(1, 100))
+               )
+               .RuleFor(o => o.Desconto, f => 40)
+               .RuleFor(o => o.Ativa, f => true);
+
+            var lista = fakerOferta.Generate(200);
+            Context.OfertasViagem.AddRange(lista);
+            Context.SaveChanges();
         }
 
         public async Task DisposeAsync()
---
+++ JM.Test.Integracao/OfertaViagemDalRecuperaMaiorDesconto.cs
@@ -0,0 +1,53 @@
+﻿using Bogus;
+using JornadaMilhas.Dados;
+using JornadaMilhasV1.Gerencidor;
+using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM.Test.Integracao
+{
+    [Collection(nameof(ContextoCollection))]
+    public class OfertaViagemDalRecuperaMaiorDesconto
+    {
+        private readonly JornadaMilhasContext context;
+        private readonly ContextoFixture fixture;
+
+        public OfertaViagemDalRecuperaMaiorDesconto(ContextoFixture fixture)
+        {
+            context = fixture.Context;
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        // destino = são paulo, desconto = 40, preco 80
+        public void RetornaOfertaEspecificaQuandoDestinoSaoPauloEDesconto40()
+        {
+            //Arrange 
+            var rota = new Rota("Curitiba", "São Paulo");
+            Periodo periodo = new(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+            fixture.CriaDadosFake();
+
+            var ofertaEscolhida = new OfertaViagem(rota, periodo, 80)
+            {
+                Desconto = 40,
+                Ativa = true
+            };
+
+            var dal = new OfertaViagemDAL(context);
+            dal.Adicionar(ofertaEscolhida);
+            Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
+            var precoEsperado = 40;
+
+            //Act 
+            var oferta = dal.RecuperaMaiorDesconto(filtro);
+
+            //Assert
+            Assert.NotNull(oferta);
+            Assert.Equal(precoEsperado, oferta.Preco, 0.0001);
+        }
+    }
+}
